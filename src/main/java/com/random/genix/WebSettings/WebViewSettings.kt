809e5364558e804fb23183c078cd2f9ab5c2cb36
randomboi404 <tanishsawant603@gmail.com>
2022-07-18 18:46:32 +0530

    GenixBrowser0

--- src/main/java/com/random/genix/WebSettings/WebViewSettings.kt
+++ src/main/java/com/random/genix/WebSettings/WebViewSettings.kt
@@ -1,3 +1,23 @@
+/*
+Copyright (C) 2022 GenixBrowser0
+
+This file is part of GenixBrowser0.
+
+GenixBrowser0 is free software:
+you can redistribute it and/or modify it under the terms of
+the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+GenixBrowser0 is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with GenixBrowser0.
+If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package com.random.genix.WebSettings
 
 import android.webkit.WebView
